a4d313044bb1fad836f619b52761795077a814b0
Simon Sjöö <simon.sjoo@forefront.se>
2025-09-08 14:41:38 +0200

    If num is 3 and 5, print fizzbuzz

--- TE-1-Fizz-Buzz/main.js
+++ TE-1-Fizz-Buzz/main.js
@@ -1,6 +1,7 @@
 for (let i = 1; i <= 100; i++) {
   let output = "";
-  if (i % 3 === 0) output += "Fizz";
-  if (i % 5 === 0) output += "Buzz";
+  if (i % 3 === 0 && i % 5 === 0) output += "FizzBuzz";
+  else if (i % 3 === 0) output += "Fizz";
+  else if (i % 5 === 0) output += "Buzz";
   console.log(output || i);
 }
